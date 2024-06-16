0bdf59e5518ebac94e8e6bbe03fe3c9a25dbc4ee
datatags <datatags@users.noreply.github.com>
2024-06-16 10:14:35 -0600

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 val pGroup = "me.gamercoder215"
-val pVersion = "1.10.2-SNAPSHOT"
+val pVersion = "1.10.4-SNAPSHOT"
 val pAuthor = "datatags" // Originally "gmitch215"
 
 val github = "$pAuthor/MobChipLite" // Originally "$pAuthor/MobChip"
